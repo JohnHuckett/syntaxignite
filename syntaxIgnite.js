af8082793fa4ff57bfddb1e45df24e00d7b64ddf
John P Huckett <john.huckett@deepfriedcircuits.com>
2015-02-13 05:48:34 +0000

    second commit

---
+++ syntaxIgnite.js
@@ -0,0 +1,136 @@
+//"use strict";
+window.onload = function(){
+	convert();
+}
+
+function convert(){
+	var bar = document.getElementsByClassName('code');
+	bar = bar.length;
+	for(var i=0;i<bar;i++){
+		var foo = document.getElementsByClassName('code')[i].id;
+		var str = document.getElementById(foo).innerHTML;
+		switch(foo){
+			case "language-c":
+				str = languageC(str);
+				document.getElementById(foo).innerHTML = str;
+				break;
+			case "language-js":
+				str = languageJs(str);
+				document.getElementById(foo).innerHTML = str;
+				break;
+			case "language-css":
+				str = languageCss(str);
+				document.getElementById(foo).innerHTML = str;
+				break;
+			case "language-html":
+				str = languageHTML(str);
+				document.getElementById(foo).innerHTML = str;
+				break;
+		}	
+	}
+
+}
+function languageHTML(str){
+	return(str)
+				//angle brackets
+				.replace(/</g, '&lt;')
+				.replace(/>/g, '&gt;')
+				//.replace(/&lt;h1&gt;/, '&lt;<span id="number">h1</span>&gt;')
+				.replace(/&lt;(.*?)&gt;/g , '<span id="number">$&</span>')
+}
+function languageCss(str){
+	return(str)	//comments
+				.replace(/\/\/(.*?)\n/ , '<span id="comment">$&</span>')
+				//class
+				.replace(/\.(.*?)[a-z|A-Z|0-9]{0,}/g , '<span id="class">$&</span>')
+				//id
+				.replace(/#(.*?){/g , '<span id="id">#$1</span>{')
+				//hex number
+				.replace(/#(.*?);/g , '<span id="number">#$1</span>;')
+				//number
+
+				//px
+				.replace(/px/,'<span id="px">$&</span>')
+				//tags
+				.replace(/\n(.*?){/g, '\n<span id="tags">$1</span>{')
+}
+
+function languageJs(str){
+
+	return(str) //angle brackets
+				.replace(/</g, '&lt;')
+				.replace(/>/g, '&gt;')
+				//strings
+				.replace(/(["'])(?:(?=(\\?))\2.)*?\1/g, "<span id=\"string\"\>$&\</span>")
+				//numbers
+				//.replace(/[0-9]/g,'<span id="number">$&</span>')
+				//comments
+				.replace(/\/\*/g,'<span id="comment">/*')
+				.replace(/\*\//g,'*/</span>')
+				.replace(/\/\/(.*?)\n/g , '<span id="comment">$&</span>')
+				//dataTypes
+			//	.replace(/var |var/g , '<span id="dataType">$&</span>')	
+				//conditional and control
+				.replace(/for\(/g ,'<span id="conditional">for</span>&#40;')
+				.replace(/for \(/g ,'<span id="conditional">for </span>&#40;')	
+				.replace(/while\(/g ,'<span id="conditional">while</span>&#40;')
+				.replace(/while \(/g ,'<span id="conditional">while</span> &#40;')
+				.replace(/if\(/g ,'<span id="conditional">if</span>&#40;')
+				.replace(/if \(/g ,'<span id="conditional">if</span> &#40;')
+				.replace(/do\{/g ,'<span id="conditional">do</span>\{')
+				.replace(/do \{/g ,'<span id="conditional">do</span> \{')
+				.replace(/return/g ,'<span id="conditional">$&</span>')
+				.replace(/break/g , '<span id="conditional">$&</span>')
+				.replace(/continue/g , '<span id="conditional">$&</span>')
+				//escape sequences
+				.replace(/\\a|\\b|\\f|\\n|\\r|\\t|\\\//g,'<span id="escape">$&</span>')
+
+}
+			
+function languageC(str){
+		
+	return (str)//angle brackets
+				.replace(/<\/.*>/, '')
+				.replace(/</g, '&lt;')
+				.replace(/>/g, '&gt;')
+				//#include part 1
+				.replace(/#include"/,'#include&quot')
+				.replace(/.h"/g,'.h&quot;</span>')
+				//strings
+				.replace(/(["'])(?:(?=(\\?))\2.)*?\1/g, "\<span id=\"string\"\>$&\<\/span\>")
+				//#include part 2
+				.replace(/#include/g,'<span id="include">#include')
+				.replace(/.h&gt;/g,'.h&gt;</span>')
+				//numbers
+				.replace(/[0-9]/g,'<span id="number">$&</span>')
+				//comments
+				.replace(/\/\*/g,'<span id="comment">/*')
+				.replace(/\*\//g,'*/</span>')
+				.replace(/\/\/(.*?)\n/ , '<span id="comment">$&</span>')
+				//place holders
+				.replace(/%d|%s|%lf|%f|%c/g,'<span id="placeHolder">$&</span>')
+				//dataTypes
+				.replace(/int |char |long |float |double |short |unsigned |signed |void /g,'<span id="dataType">$&</span>')
+				//conditional and control
+				.replace(/for\(/g ,'<span id="conditional">for</span>&#40;')
+				.replace(/for \(/g ,'<span id="conditional">for </span>&#40;')	
+				.replace(/while\(/g ,'<span id="conditional">while</span>&#40;')
+				.replace(/while \(/g ,'<span id="conditional">while</span> &#40;')
+				.replace(/if\(/g ,'<span id="conditional">if</span>&#40;')
+				.replace(/if \(/g ,'<span id="conditional">if</span> &#40;')
+				.replace(/do\{/g ,'<span id="conditional">do</span>\{')
+				.replace(/do \{/g ,'<span id="conditional">do</span> \{')
+				.replace(/return/g ,'<span id="conditional">$&</span>')
+				.replace(/break/g , '<span id="conditional">$&</span>')
+				.replace(/continue/g , '<span id="conditional">$&</span>')
+				//escape sequences
+				.replace(/\\a|\\b|\\f|\\n|\\r|\\t|\\/,'<span id="escape">$&</span>')
+				//functions	
+				//.replace(/\t(.*?)\(/gm , '\t<span id="number">$1</span>(')
+				//.replace(/void(.*?)\(/gm , 'void<span id="number">$1</span>(')
+}
+
+function consoleLog (argument) {
+	var str = document.getElementById("language-c").innerHTML;
+	console.log(str.split(/[""]/g));
+}
